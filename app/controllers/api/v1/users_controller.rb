7daacd2f6825ed2c001d0c8e0c281b5c42b6451a
Trunkg0d <trungsanglong@gmail.com>
2022-11-26 14:15:31 +0700

    Update user

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,8 +1,9 @@
 class Api::V1::UsersController < ApplicationController
   before_action :authenticate_user!, only: [:show, :index]
+  before_action :check_owner, only: [:update, :destroy]
 
   def show
-    @user = User.find_by(id: params[:id])k
+    @user = User.find_by(id: params[:id])
     if @user
       render json: {
         data: @user
@@ -25,5 +26,34 @@ class Api::V1::UsersController < ApplicationController
         messages: "Can not show user"
       }, status: :not_found
     end
+  end
+
+  def update
+    @user = User.find_by(id: params[:id])
+    if @user.update(user_params)
+      render json: {
+        messages: "User updated successfully",
+        data: @user
+      }, status: :ok
+    else
+      render json: {
+        messages: "Can not update user",
+        data: @user.errors
+      }, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :avatar, :email, :phone)
+  end
+
+  def check_owner
+    @user = User.find_by(id: params[:id])
+    head :forbidden unless @user.id == current_user.id
   end
 end
